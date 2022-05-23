2fa882f641e96aa5c59e4460126dcc6e0f4f3c3a
usama <mahmoodify@gmail.com>
2022-05-23 19:00:20 +0500

    Adding web pipeline orchestration helpers

---
+++ src/ACMEv2/WebInterface/Pipeline.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CronBlocks.ACMEv2.WebInterface
+{
+    public static class Pipeline
+    {
+        public static void AddACMEv2(this IServiceCollection services)
+        {
+
+        }
+
+        public static void UseACMEv2(this IApplicationBuilder builder)
+        {
+
+        }
+    }
+}
