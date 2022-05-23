d4ee1d46d00dd669d9d3e78591d221cb20b14880
usama <mahmoodify@gmail.com>
2022-05-23 18:43:10 +0500

    Adding default webapp

---
+++ src/ACMEv2WebApp/Program.cs
@@ -0,0 +1,37 @@
+namespace ACMEv2WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+
+            //
+            // Add services to the container
+            //
+            builder.Services.AddRazorPages();
+
+            WebApplication? app = builder.Build();
+
+            //
+            // Configure the HTTP request pipeline
+            //
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
