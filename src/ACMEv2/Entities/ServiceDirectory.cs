c6638d173752e0d6dcb62e23ee0a69ddafca0919
usama <mahmoodify@gmail.com>
2022-05-24 20:49:07 +0500

    Adding setting for service directory and svc dir entity

---
+++ src/ACMEv2/Entities/ServiceDirectory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CronBlocks.ACMEv2.Entities
+{
+    public class ServiceDirectory
+    {
+        public Dictionary<string, object> extra { get; set; } = new Dictionary<string, object>();
+        public string Directory { get; set; }
+        public string NewNonce { get; set; }
+        public string NewAccount { get; set; }
+        public string NewOrder { get; set; }
+        public string NewAuthz { get; set; }
+        public string RevokeCert { get; set; }
+        public string KeyChange { get; set; }
+        public ServiceDirectoryMeta Meta { get; set; }
+    }
+}
--- src/ACMEv2/Settings/LetsEncryptSettings.cs
+++ src/ACMEv2/Settings/LetsEncryptSettings.cs
@@ -4,5 +4,12 @@
     {
         public static readonly string Agreement = "https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf";
         public static readonly string ProblemTypeNamespace = "urn:ietf:params:acme:error:";
+        public static readonly string Directory = "directory";
+        public static readonly string NewNonce = "acme/new-nonce";
+        public static readonly string NewAccount = "acme/new-acct";
+        public static readonly string NewOrder = "acme/new-order";
+        public static readonly string NewAuthz = "acme/new-authz";
+        public static readonly string RevokeCert = "acme/revoke-cert";
+        public static readonly string KeyChange = "acme/key-change";
     }
 }
