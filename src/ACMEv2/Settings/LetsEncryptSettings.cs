1a024624d4e3be7256b1d6df5013779eda1accf3
usama <mahmoodify@gmail.com>
2022-05-24 20:20:44 +0500

    Adding problem type namespace property

--- src/ACMEv2/Settings/LetsEncryptSettings.cs
+++ src/ACMEv2/Settings/LetsEncryptSettings.cs
@@ -7,5 +7,6 @@ namespace CronBlocks.ACMEv2.Settings
     public class LetsEncryptSettings
     {
         public static readonly string Agreement = "https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf";
+        public static readonly string ProblemTypeNamespace = "urn:ietf:params:acme:error:";
     }
 }
