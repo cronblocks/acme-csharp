06a32c47073360c2953b2dea95b0af16056b0000
usama <mahmoodify@gmail.com>
2022-05-24 20:22:14 +0500

    Adding problem namespace

---
+++ src/ACMEv2/Entities/Problem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CronBlocks.ACMEv2.Entities
+{
+    public class Problem
+    {
+        public string StandardProblemTypeNamespace { get; set; }
+        public string Type { get; set; }
+        public string Detail { get; set; }
+        public int? Status { get; set; }
+    }
+}
