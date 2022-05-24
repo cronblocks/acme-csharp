c3a05a411dce88a718b64a63cd4035ff5d6d2793
usama <mahmoodify@gmail.com>
2022-05-24 20:03:47 +0500

    Making plain entity for authorization

--- src/ACMEv2/Entities/AuthorizationDetails.cs
+++ src/ACMEv2/Entities/AuthorizationDetails.cs
@@ -6,21 +6,10 @@ namespace CronBlocks.ACMEv2.Entities
 {
     public class AuthorizationDetails
     {
-        [JsonProperty("identifier", Required = Required.Always)]
         public Identifier Identifier { get; set; }
-
-        [JsonProperty("status", Required = Required.Always)]
-        [Required]
         public string Status { get; set; }
-
-        [JsonProperty("expires")]
         public string Expires { get; set; }
-
-        [JsonProperty("challenges")]
-        [Required]
         public Challenge[] Challenges { get; set; }
-
-        [JsonProperty("wildcard")]
         public bool? Wildcard { get; set; }
     }
 }
