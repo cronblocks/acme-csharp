b411f1b94e1f5de938f5dc609cd87197dda26106
usama <mahmoodify@gmail.com>
2022-05-24 20:50:31 +0500

    Adding props for service directory meta

--- src/ACMEv2/Entities/ServiceDirectoryMeta.cs
+++ src/ACMEv2/Entities/ServiceDirectoryMeta.cs
@@ -6,5 +6,9 @@ namespace CronBlocks.ACMEv2.Entities
 {
     public class ServiceDirectoryMeta
     {
+        public string TermsOfService { get; set; }
+        public string Website { get; set; }
+        public string[] CaaIdentities { get; set; }
+        public string ExternalAccountRequired { get; set; }
     }
 }
