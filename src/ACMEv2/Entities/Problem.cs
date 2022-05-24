929a9b9e6a3ca1c11d31b6526c4251786094e0df
usama <mahmoodify@gmail.com>
2022-05-24 20:24:07 +0500

    Adding problem type enum

--- src/ACMEv2/Entities/Problem.cs
+++ src/ACMEv2/Entities/Problem.cs
@@ -7,8 +7,36 @@ namespace CronBlocks.ACMEv2.Entities
     public class Problem
     {
         public string StandardProblemTypeNamespace { get; set; }
-        public string Type { get; set; }
+        public ProblemType Type { get; set; }
         public string Detail { get; set; }
         public int? Status { get; set; }
+    }
+
+    public enum ProblemType
+    {
+        Unknown = 0,
+
+        AccountDoesNotExist,
+        AlreadyRevoked,
+        BadCSR,
+        BadNonce,
+        BadRevocationReason,
+        BadSignatureAlgorithm,
+        Caa,
+        Compound,
+        Connection,
+        Dns,
+        ExternalAccountRequired,
+        IncorrectResponse,
+        InvalidContact,
+        Malformed,
+        RateLimited,
+        RejectedIdentifier,
+        ServerInternal,
+        Tls,
+        Unauthorized,
+        UnsupportedContact,
+        UnsupportedIdentifier,
+        UserActionRequired,
     }
 }
