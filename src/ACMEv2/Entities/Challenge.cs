f614cfdcd67afee76fb01198a079c5c08ee8004d
usama <mahmoodify@gmail.com>
2022-05-24 20:08:05 +0500

    Adding challenge properties

--- src/ACMEv2/Entities/Challenge.cs
+++ src/ACMEv2/Entities/Challenge.cs
@@ -6,5 +6,12 @@ namespace CronBlocks.ACMEv2.Entities
 {
     public class Challenge
     {
+        public string Type { get; set; }
+        public string Url { get; set; }
+        public string Status { get; set; }
+        public string Validated { get; set; }
+        public object[] ValidationRecord { get; set; }
+        public object Error { get; set; }
+        public string Token { get; set; }
     }
 }
