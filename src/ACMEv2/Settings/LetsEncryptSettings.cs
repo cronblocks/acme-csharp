86d13086b5791750c5c6e15aed2c3caa8d6567ad
usama <mahmoodify@gmail.com>
2022-05-24 20:16:46 +0500

    Adding lets encrypt settings

---
+++ src/ACMEv2/Settings/LetsEncryptSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CronBlocks.ACMEv2.Settings
+{
+    public class LetsEncryptSettings
+    {
+        public static readonly string Agreement = "https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf";
+    }
+}
