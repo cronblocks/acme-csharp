729b090b7b7fa069316412f4bb8f48f623594922
usama <mahmoodify@gmail.com>
2022-05-24 20:13:35 +0500

    Adding account properties

--- src/ACMEv2/Entities/Account.cs
+++ src/ACMEv2/Entities/Account.cs
@@ -6,5 +6,14 @@ namespace CronBlocks.ACMEv2.Entities
 {
     public class Account
     {
+        public string Id { get; set; }
+        public object Key { get; set; }
+        public string[] Contact { get; set; }
+        public string Status { get; set; }
+        public bool? TermsOfServiceAgreed { get; set; }
+        public string Orders { get; set; }
+        public string InitialIp { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Agreement { get; set; }
     }
 }
