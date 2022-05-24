1c9d716391cdb287fe43f082114d8248c1a3f83d
usama <mahmoodify@gmail.com>
2022-05-24 20:28:17 +0500

    Adding revoke reason enum

---
+++ src/ACMEv2/Entities/RevokeReason.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CronBlocks.ACMEv2.Entities
+{
+    public enum RevokeReason
+    {
+        Unspecified = 0,
+        KeyCompromise,
+        CaCompromise,
+        AffiliationChanged,
+        Superseded,
+        CessationOfOperation,
+        CertificateHold,
+        
+        _UNUSED,
+
+        RemoveFromCrl,
+        PrivilegeWithdrawn,
+        AaCompromise
+    }
+}
