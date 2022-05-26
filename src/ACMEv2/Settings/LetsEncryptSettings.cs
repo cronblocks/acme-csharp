0df5e47ef34ce5b2ac26291ce7793380f031fd81
usama <mahmoodify@gmail.com>
2022-05-26 20:02:45 +0500

    Changing scope of letsenc settings to internal

--- src/ACMEv2/Settings/LetsEncryptSettings.cs
+++ src/ACMEv2/Settings/LetsEncryptSettings.cs
@@ -1,6 +1,6 @@
 ﻿namespace CronBlocks.ACMEv2.Settings
 {
-    public class LetsEncryptSettings
+    internal class LetsEncryptSettings
     {
         public static readonly string Agreement = "https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf";
         public static readonly string ProblemTypeNamespace = "urn:ietf:params:acme:error:";
