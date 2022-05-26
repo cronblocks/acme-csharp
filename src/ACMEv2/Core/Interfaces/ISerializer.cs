a24acb9127a969ed340e68afe5222d2beb1a6b6f
usama <mahmoodify@gmail.com>
2022-05-26 23:29:23 +0500

    Cleaning up serialization iface

--- src/ACMEv2/Core/Interfaces/ISerializer.cs
+++ src/ACMEv2/Core/Interfaces/ISerializer.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CronBlocks.ACMEv2.Core.Interfaces
+﻿namespace CronBlocks.ACMEv2.Core.Interfaces
 {
     internal interface ISerializer
     {
