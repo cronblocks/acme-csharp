b830bc38ac89d5e1451d21b54f4586b3f1531c80
usama <mahmoodify@gmail.com>
2022-05-26 23:27:51 +0500

    Impleenting serialization

--- src/ACMEv2/Core/DefaultInterfacesImpl/Serializer.cs
+++ src/ACMEv2/Core/DefaultInterfacesImpl/Serializer.cs
@@ -8,5 +8,24 @@ namespace CronBlocks.ACMEv2.Core.DefaultInterfacesImpl
 {
     internal class Serializer : ISerializer
     {
+        public string Serialize<T>(T obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+
+            return JsonSerializer.Serialize(obj);
+        }
+
+        public T Deserialize<T>(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return default(T);
+            }
+
+            return JsonSerializer.Deserialize<T>(json);
+        }
     }
 }
--- src/ACMEv2/Core/Interfaces/ISerializer.cs
+++ src/ACMEv2/Core/Interfaces/ISerializer.cs
@@ -6,5 +6,7 @@ namespace CronBlocks.ACMEv2.Core.Interfaces
 {
     internal interface ISerializer
     {
+        string Serialize<T>(T obj);
+        T Deserialize<T>(string json);
     }
 }
