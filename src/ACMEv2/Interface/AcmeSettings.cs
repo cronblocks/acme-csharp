bf0d8ea2a9113adb43bdcbd6ddcac8eaacffe515
usama <mahmoodify@gmail.com>
2022-05-31 18:14:13 +0500

    Moving ACME settings to interface ns

---
+++ src/ACMEv2/Interface/AcmeSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CronBlocks.ACMEv2.Interface
+{
+    public enum AcmeProvider
+    {
+        LetsEncrypt
+    }
+
+    public class AcmeSettings
+    {
+        /// <summary>
+        /// Directory where local ACME data is kept.
+        /// </summary>
+        public string LocalDataDir { get; set; } = $"{Path.Combine(Environment.CurrentDirectory, ".acme-data")}";
+
+        /// <summary>
+        /// Provider - default is Let's Encrypt
+        /// </summary>
+        public AcmeProvider AcmeProvider { get; set; } = AcmeProvider.LetsEncrypt;
+
+        /// <summary>
+        /// Whether or not to use staging server.
+        /// </summary>
+        public bool UseStaging { get; set; } = true;
+    }
+}
