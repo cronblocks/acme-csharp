ca4c443539f17e976eab1c9b8723d4476a51b4fe
usama <mahmoodify@gmail.com>
2022-05-31 21:01:22 +0500

    Implementing IServer in temp server

--- src/ACMEv2/Core/TempServer/TempServer.cs
+++ src/ACMEv2/Core/TempServer/TempServer.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using CronBlocks.ACMEv2.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CronBlocks.ACMEv2.Core.TempServer
 {
-    internal class TempServer
+    internal class TempServer : IServer
     {
     }
 }
