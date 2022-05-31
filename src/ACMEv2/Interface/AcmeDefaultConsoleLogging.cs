53ff9ed138ac60a98aba459ee7b9b2e9f6a4034b
usama <mahmoodify@gmail.com>
2022-05-31 19:29:52 +0500

    Adding impl for default logging

---
+++ src/ACMEv2/Interface/AcmeDefaultConsoleLogging.cs
@@ -0,0 +1,40 @@
+﻿namespace CronBlocks.ACMEv2.Interface
+{
+    internal class AcmeDefaultConsoleLogging : IAcmeLogging
+    {
+        private readonly bool logInfo;
+        private readonly bool logWarning;
+        private readonly bool logError;
+
+        public AcmeDefaultConsoleLogging(bool logInfo, bool logWarning, bool logError)
+        {
+            this.logInfo = logInfo;
+            this.logWarning = logWarning;
+            this.logError = logError;
+        }
+
+        public void LogError(string message)
+        {
+            if (logError)
+            {
+                System.Console.WriteLine($"ACME ERROR: {message}");
+            }
+        }
+
+        public void LogInfo(string message)
+        {
+            if (logInfo)
+            {
+                System.Console.WriteLine($"ACME: {message}");
+            }
+        }
+
+        public void LogWarning(string message)
+        {
+            if (logWarning)
+            {
+                System.Console.WriteLine($"ACME Warning: {message}");
+            }
+        }
+    }
+}
