73eda007f58c386695f3b2531b32a49bafce910f
usama <mahmoodify@gmail.com>
2022-05-31 18:01:07 +0500

    Adding settings for provider and staging

--- src/ACMEv2/Interface/KestrelPipeline/AcmeSettings.cs
+++ src/ACMEv2/Interface/KestrelPipeline/AcmeSettings.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace CronBlocks.ACMEv2.Interface.KestrelPipeline
 {
+    public enum AcmeProvider
+    {
+        LetsEncrypt
+    }
+
     public class AcmeSettings
     {
+        public AcmeProvider AcmeProvider { get; set; } = AcmeProvider.LetsEncrypt;
+        public bool UseStaging { get; set; } = true;
     }
 }
