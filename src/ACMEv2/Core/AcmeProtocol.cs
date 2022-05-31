2ed8d0a2167b01c5c8088a4489eb162b07bd3dc4
usama <mahmoodify@gmail.com>
2022-05-31 19:40:47 +0500

    Adding ctor for protocol by passing settings and logging interface

--- src/ACMEv2/Core/AcmeProtocol.cs
+++ src/ACMEv2/Core/AcmeProtocol.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CronBlocks.ACMEv2.Interface;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,13 @@ namespace CronBlocks.ACMEv2.Core
 {
     internal class AcmeProtocol
     {
+        private readonly IAcmeLogging _log;
+        private readonly CoreSettings _settings;
+
+        public AcmeProtocol(IAcmeLogging logging, CoreSettings settings)
+        {
+            _log = logging ?? throw new ArgumentNullException(nameof(logging));
+            _settings = settings ?? throw new ArgumentNullException(nameof(settings));
+        }
     }
 }
