fa9785c9b72b3e74b8956576a2779063fbd82129
usama <mahmoodify@gmail.com>
2022-05-31 19:19:17 +0500

    Adding logging methods

--- src/ACMEv2/Interface/IAcmeLogging.cs
+++ src/ACMEv2/Interface/IAcmeLogging.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CronBlocks.ACMEv2.Interface
+﻿namespace CronBlocks.ACMEv2.Interface
 {
     public interface IAcmeLogging
     {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
     }
 }
