c3dfab0e48c2eeecb98501edf3277910292aadd2
usama <mahmoodify@gmail.com>
2022-06-01 23:24:17 +0500

    Adding logging level enum

--- src/ACMEv2/Interface/IAcmeLogging.cs
+++ src/ACMEv2/Interface/IAcmeLogging.cs
@@ -1,7 +1,13 @@
 ﻿namespace CronBlocks.ACMEv2.Interface
 {
+    public enum LoggingLevel
+    {
+        Info, Warning, Error
+    }
+
     public interface IAcmeLogging
     {
+        void SetLoggingLevel(LoggingLevel level);
         void LogInfo(string message);
         void LogWarning(string message);
         void LogError(string message);
