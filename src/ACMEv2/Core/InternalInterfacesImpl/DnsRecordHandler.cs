f793a6dcf03d3eca524f0a17eef5fa0d4eb0b5fd
usama <mahmoodify@gmail.com>
2022-06-02 00:04:55 +0500

    Adding blank dns handler

---
+++ src/ACMEv2/Core/InternalInterfacesImpl/DnsRecordHandler.cs
@@ -0,0 +1,11 @@
+﻿using CronBlocks.ACMEv2.Core.InternalInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CronBlocks.ACMEv2.Core.InternalInterfacesImpl
+{
+    internal class DnsRecordHandler : IDnsRecordHandler
+    {
+    }
+}
