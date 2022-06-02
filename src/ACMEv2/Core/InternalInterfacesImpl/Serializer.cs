1e320d98ee76a9737ca1217be4e1340a2421d5d1
usama <mahmoodify@gmail.com>
2022-06-02 18:44:16 +0500

    Adding implementation for b64 handling

--- src/ACMEv2/Core/InternalInterfacesImpl/Serializer.cs
+++ src/ACMEv2/Core/InternalInterfacesImpl/Serializer.cs
@@ -1,4 +1,6 @@
 ﻿using CronBlocks.ACMEv2.Core.InternalInterfaces;
+using System;
+using System.Text;
 using System.Text.Json;
 
 namespace CronBlocks.ACMEv2.Core.InternalInterfacesImpl
@@ -23,6 +25,28 @@ namespace CronBlocks.ACMEv2.Core.InternalInterfacesImpl
             }
 
             return JsonSerializer.Deserialize<T>(json);
+        }
+
+        public string SerializeToBase64(byte[] bytes, bool trimTrailingEquals = false)
+        {
+            if (bytes == null)
+            {
+                return "";
+            }
+
+            string b64 = Convert.ToBase64String(bytes, 0, bytes.Length);
+
+            return trimTrailingEquals ? b64.Replace("=", "") : b64;
+        }
+
+        public byte[] DeserializeFromBase64(string serializedBase64)
+        {
+            if (string.IsNullOrEmpty(serializedBase64))
+            {
+                return null;
+            }
+
+            return Convert.FromBase64String(serializedBase64);
         }
     }
 }
