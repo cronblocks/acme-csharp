84bd1b077060215ff4637d901f1386ef23d65cab
usama <mahmoodify@gmail.com>
2022-06-02 19:56:57 +0500

    Adding helpers for conversions related to base 64

---
+++ src/ACMEv2/Core/Helpers/ConversionHelpers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CronBlocks.ACMEv2.Core.Helpers
+{
+    internal static class ConversionHelpers
+    {
+        public static string ConvertToBase64(this string data, bool removeTrailingEquals = true)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                return "";
+            }
+
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+
+            return ConvertToBase64(bytes, removeTrailingEquals);
+        }
+
+        public static string ConvertToBase64(this byte[] bytes, bool removeTrailingEquals = true)
+        {
+            if (bytes == null)
+            {
+                return "";
+            }
+
+            return ConvertToBase64(bytes, 0, bytes.Length, removeTrailingEquals);
+        }
+
+        public static string ConvertToBase64(this byte[] bytes, int start, int total, bool removeTrailingEquals = true)
+        {
+            if (bytes == null)
+            {
+                return "";
+            }
+
+            string b64 = Convert.ToBase64String(bytes, start, total, Base64FormattingOptions.None);
+
+            return removeTrailingEquals ? b64.Replace("=", "") : b64;
+        }
+
+        public static string ConvertFromBase64(this string serializedBase64)
+        {
+            if (string.IsNullOrEmpty(serializedBase64))
+            {
+                return "";
+            }
+
+            return Encoding.UTF8.GetString(Convert.FromBase64String(serializedBase64));
+        }
+    }
+}
