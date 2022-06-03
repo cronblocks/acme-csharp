be907de877161cb15c5f2c1ffb6179c5aefbf8ea
usama <mahmoodify@gmail.com>
2022-06-03 21:08:44 +0500

    Adding problem type extensions

---
+++ src/ACMEv2/Core/Extensions/ProblemTypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CronBlocks.ACMEv2.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CronBlocks.ACMEv2.Core.Extensions
+{
+    internal static class ProblemTypeExtensions
+    {
+        public static string Description(this ProblemType problemType)
+        {
+            return "Unknown";
+        }
+    }
+}
