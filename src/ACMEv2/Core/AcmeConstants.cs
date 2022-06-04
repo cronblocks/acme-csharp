5b25eefd31a6d37624a5f276fb827eada177379e
usama <mahmoodify@gmail.com>
2022-06-04 19:42:23 +0500

    Adding well known path prefix

--- src/ACMEv2/Core/AcmeConstants.cs
+++ src/ACMEv2/Core/AcmeConstants.cs
@@ -3,5 +3,6 @@
     internal class AcmeConstants
     {
         public static readonly string ProblemTypeNamespace = "urn:ietf:params:acme:error:";
+        public static readonly string WellKnownPathPrefix = "/.well-known/";
     }
 }
