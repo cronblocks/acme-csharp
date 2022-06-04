96cff5121db06ed96270f7a18741a344cff443e6
usama <mahmoodify@gmail.com>
2022-06-04 19:13:52 +0500

    Removing error namespace from lets enc specific settings

--- src/ACMEv2/Core/LetsEncryptSettings.cs
+++ src/ACMEv2/Core/LetsEncryptSettings.cs
@@ -6,7 +6,6 @@
         public static readonly string AcmeServerStaging = "https://acme-staging-v02.api.letsencrypt.org/directory";
 
         public static readonly string Agreement = "https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf";
-        public static readonly string ProblemTypeNamespace = "urn:ietf:params:acme:error:";
         public static readonly string Directory = "directory";
         public static readonly string NewNonce = "acme/new-nonce";
         public static readonly string NewAccount = "acme/new-acct";
