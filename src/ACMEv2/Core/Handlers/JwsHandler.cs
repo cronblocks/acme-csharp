a12dacc85610b0cbe656f3b2b702bb07aa74d592
usama <mahmoodify@gmail.com>
2022-06-05 12:13:10 +0500

    Adjusting line widths

--- src/ACMEv2/Core/Handlers/JwsHandler.cs
+++ src/ACMEv2/Core/Handlers/JwsHandler.cs
@@ -5,8 +5,8 @@ namespace CronBlocks.ACMEv2.Core.Handlers
     /// <summary>
     /// ACME Specific implementation is emphasized:
     /// 
-    ///     A JWS object sent as the body of an ACME request MUST meet the
-    /// following additional criteria:
+    /// A JWS object sent as the body of an ACME request MUST meet the following additional
+    /// criteria:
     /// 
     ///     o  The JWS MUST be in the Flattened JSON Serialization [RFC7515]
     ///     o  The JWS MUST NOT have multiple signatures
@@ -17,29 +17,29 @@ namespace CronBlocks.ACMEv2.Core.Handlers
     ///     
     ///        *  "alg" (Algorithm)
     ///        
-    ///           +  This field MUST NOT contain "none" or a Message Authentication
-    ///              Code (MAC) algorithm (e.g.one in which the algorithm registry
-    ///              description mentions MAC/HMAC)
+    ///           +  This field MUST NOT contain "none" or a Message Authentication Code (MAC)
+    ///              algorithm (e.g., one in which the algorithm registry description mentions
+    ///              MAC / HMAC)
     ///              
     ///        *  "nonce" (defined in Section 6.5)
     ///        *  "url" (defined in Section 6.4)
-    ///        *  "jwk" or "kid" - mutually exclusive.
+    ///        *  "jwk" or "kid" - mutually exclusive
     ///           
     ///           +  "jwk" (JSON Web Key)
     ///           
-    ///              -  For newAccount requests, and for revokeCert requests authenticated by
-    ///                 a certificate key, there MUST be a "jwk" field. This field MUST contain the
-    ///                 public key corresponding to the private key used to sign the JWS.
+    ///              -  For newAccount requests, and for revokeCert requests authenticated by a
+    ///                 certificate key, there MUST be a "jwk" field. This field MUST contain the
+    ///                 public key corresponding to the private key used to sign the JWS
     ///                 
     ///           +  "kid" (Key ID)
     ///           
     ///              -  For all other requests, the request is signed using an existing account,
     ///                 and there MUST be a "kid" field. This field MUST contain the account URL
-    ///                 received by POSTing to the newAccount resource.
+    ///                 received by POSTing to the newAccount resource
     ///                 
-    ///     o  An ACME server MUST implement the "ES256" signature algorithm [RFC7518]
-    ///        and SHOULD implement the "EdDSA" signature algorithm using the "Ed25519"
-    ///        variant (indicated by "crv") [RFC8037]
+    ///     o  An ACME server MUST implement the "ES256" signature algorithm [RFC7518] and SHOULD
+    ///        implement the "EdDSA" signature algorithm using the "Ed25519" variant (indicated by
+    ///        "crv") [RFC8037]
     /// </summary>
     internal class JwsHandler
     {
