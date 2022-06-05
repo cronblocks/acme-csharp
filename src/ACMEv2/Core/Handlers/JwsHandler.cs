fa388180bf94638bb5f45790b9f3743a4dffa12e
usama <mahmoodify@gmail.com>
2022-06-05 12:27:48 +0500

    Removing json serialization separate

--- src/ACMEv2/Core/Handlers/JwsHandler.cs
+++ src/ACMEv2/Core/Handlers/JwsHandler.cs
@@ -44,24 +44,6 @@ namespace CronBlocks.ACMEv2.Core.Handlers
     /// </summary>
     internal class JwsHandler
     {
-        public string SerializeToJson<T>(T obj)
-        {
-            if (obj == null)
-            {
-                return null;
-            }
-
-            return JsonSerializer.Serialize(obj);
-        }
-
-        public T DeserializeFromJson<T>(string json)
-        {
-            if (string.IsNullOrEmpty(json))
-            {
-                return default(T);
-            }
-
-            return JsonSerializer.Deserialize<T>(json);
-        }
+        
     }
 }
