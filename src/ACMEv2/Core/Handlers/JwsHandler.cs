c9e89e8b5ec6c0f96734fd6bc2c8d840caad38f1
usama <mahmoodify@gmail.com>
2022-06-05 12:01:54 +0500

    Fmt clarity

--- src/ACMEv2/Core/Handlers/JwsHandler.cs
+++ src/ACMEv2/Core/Handlers/JwsHandler.cs
@@ -14,22 +14,29 @@ namespace CronBlocks.ACMEv2.Core.Handlers
     ///     o  The JWS Unprotected Header [RFC7515] MUST NOT be used
     ///     o  The JWS Payload MUST NOT be detached
     ///     o  The JWS Protected Header MUST include the following fields:
+    ///     
     ///        *  "alg" (Algorithm)
+    ///        
     ///           +  This field MUST NOT contain "none" or a Message Authentication
     ///              Code (MAC) algorithm (e.g.one in which the algorithm registry
     ///              description mentions MAC/HMAC)
+    ///              
     ///        *  "nonce" (defined in Section 6.5)
     ///        *  "url" (defined in Section 6.4)
-    ///        *  Either "jwk" (JSON Web Key) or "kid" (Key ID) as specified -
-    ///           fields are mutually exclusive.
+    ///        *  "jwk" (JSON Web Key) or "kid" (Key ID) - mutually exclusive.
+    ///           
     ///           +  "jwk"
+    ///           
     ///              -  For newAccount requests, and for revokeCert requests authenticated by
     ///                 a certificate key, there MUST be a "jwk" field. This field MUST contain the
     ///                 public key corresponding to the private key used to sign the JWS.
+    ///                 
     ///           +  "kid"
+    ///           
     ///              -  For all other requests, the request is signed using an existing account,
     ///                 and there MUST be a "kid" field. This field MUST contain the account URL
     ///                 received by POSTing to the newAccount resource.
+    ///                 
     ///     o  An ACME server MUST implement the "ES256" signature algorithm [RFC7518]
     ///        and SHOULD implement the "EdDSA" signature algorithm using the "Ed25519"
     ///        variant (indicated by "crv") [RFC8037]
