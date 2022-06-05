04f7dd578be9cda201a64c0ad43690a1834b4c6c
usama <mahmoodify@gmail.com>
2022-06-05 20:25:14 +0500

    Adding logging

--- src/ACMEv2/Core/Services/ApiClient.cs
+++ src/ACMEv2/Core/Services/ApiClient.cs
@@ -1,3 +1,4 @@
+﻿using CronBlocks.ACMEv2.Interface;
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,5 +7,11 @@ namespace CronBlocks.ACMEv2.Core.Services
 {
     internal class ApiClient
     {
+        private readonly ILogging _log;
+
+        public ApiClient(ILogging logging)
+        {
+            _log = logging;
     }
+}
 }
